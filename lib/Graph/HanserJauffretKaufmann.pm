b90447d2c52dce07a84c4b99cdf23028b34bb181
Andrius Merkys <andrius.merkys@gmail.com>
2023-01-13 08:53:42 +0200

    Adding some comments.

--- lib/Graph/HanserJauffretKaufmann.pm
+++ lib/Graph/HanserJauffretKaufmann.pm
@@ -22,6 +22,7 @@ sub find_cycles
         my @edges;
         my @loops;
         for my $edge ($p->edges_at( $vertex )) {
+            # Edges that start and end at the same vertex are self-loops
             if( $edge->[0] ne $edge->[1] ) {
                 push @edges, map { [ @$edge, $_ ] } $p->get_multiedge_ids( @$edge );
             } else {
@@ -43,8 +44,11 @@ sub find_cycles
                     push @new_path,
                          @{$p->get_edge_attribute_by_id( @{$edges[$j]}, 'path' )};
                 }
+                # If paths have more vertices in common than $vertex, they have to be eliminated
                 next unless scalar( uniq @new_path ) == scalar( @new_path );
-                my @new_edge = grep { $_ ne $vertex } map { $_->[0], $_->[1] } ( $edges[$i], $edges[$j] );
+                my @new_edge = grep { $_ ne $vertex }
+                                map { $_->[0], $_->[1] }
+                                    ( $edges[$i], $edges[$j] );
                 my $edge = $p->add_edge_get_id( @new_edge );
                 $p->set_edge_attribute_by_id( @new_edge, $edge, 'path', \@new_path );
             }
