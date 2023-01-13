6a2a4227890b995c86950eb24b455557d0fcdb73
Andrius Merkys <andrius.merkys@gmail.com>
2023-01-13 09:47:16 +0200

    Making the code even more effective by circumventing the internal Graph attribute handling.

--- lib/Graph/HanserJauffretKaufmann.pm
+++ lib/Graph/HanserJauffretKaufmann.pm
@@ -20,6 +20,8 @@ sub find_cycles
     # Vertices have to be visited in the order of increasing degree
     my @order = sort { $p->degree( $a ) <=> $p->degree( $b ) } $p->vertices;
 
+    my $attributes = {};
+
     my @cycles;
     for my $vertex (@order) {
         my @edges;
@@ -27,26 +29,33 @@ sub find_cycles
         for my $edge ($p->edges_at( $vertex )) {
             # Edges that start and end at the same vertex are self-loops
             if( $edge->[0] ne $edge->[1] ) {
-                push @edges, map { [ @$edge, $_ ] } $p->get_multiedge_ids( @$edge );
+                push @edges, map { [ sort( @$edge ), $_ ] } $p->get_multiedge_ids( @$edge );
             } else {
-                push @loops, map { [ @$edge, $_ ] } $p->get_multiedge_ids( @$edge );
+                push @loops, map { [ sort( @$edge ), $_ ] } $p->get_multiedge_ids( @$edge );
             }
         }
         for my $loop (@loops) {
-            push @cycles, [ $vertex, @{$p->get_edge_attribute_by_id( @$loop, 'path' )} ];
+            push @cycles, [ $vertex, @{$attributes->{$loop->[0]}{$loop->[1]}{$loop->[2]}} ];
         }
         for my $i (0..$#edges) {
             for my $j ($i+1..$#edges) {
-                my $path_i = $p->get_edge_attribute_by_id( @{$edges[$i]}, 'path' );
-                my $path_j = $p->get_edge_attribute_by_id( @{$edges[$j]}, 'path' );
-                my @new_path = (defined $path_i ? @$path_i : (), $vertex, defined $path_j ? @$path_j : ());
+                my @new_path;
+                if( exists $attributes->{$edges[$i]->[0]}{$edges[$i]->[1]}{$edges[$i]->[2]} ) {
+                    push @new_path,
+                         @{$attributes->{$edges[$i]->[0]}{$edges[$i]->[1]}{$edges[$i]->[2]}};
+                }
+                push @new_path, $vertex;
+                if( exists $attributes->{$edges[$j]->[0]}{$edges[$j]->[1]}{$edges[$j]->[2]} ) {
+                    push @new_path,
+                         @{$attributes->{$edges[$j]->[0]}{$edges[$j]->[1]}{$edges[$j]->[2]}};
+                }
                 # If paths have more vertices in common than $vertex, they have to be eliminated
                 next unless scalar( uniq @new_path ) == scalar( @new_path );
-                my @new_edge = grep { $_ ne $vertex }
+                my @new_edge = sort grep { $_ ne $vertex }
                                 map { $_->[0], $_->[1] }
                                     ( $edges[$i], $edges[$j] );
                 my $edge = $p->add_edge_get_id( @new_edge );
-                $p->set_edge_attribute_by_id( @new_edge, $edge, 'path', \@new_path );
+                $attributes->{$new_edge[0]}{$new_edge[1]}{$edge} = \@new_path;
             }
         }
         $p->delete_vertex( $vertex );
