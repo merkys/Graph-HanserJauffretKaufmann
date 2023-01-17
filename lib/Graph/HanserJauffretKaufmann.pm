b1071e2f969fe204c5371ed12900bb3992e07909
Andrius Merkys <andrius.merkys@gmail.com>
2023-01-17 12:59:26 +0200

    Saving a little time by removing some temporary variables.

--- lib/Graph/HanserJauffretKaufmann.pm
+++ lib/Graph/HanserJauffretKaufmann.pm
@@ -38,20 +38,16 @@ sub find_cycles
             push @cycles, [ $vertex, keys %{$attributes->{$loop->[0]}{$loop->[1]}{$loop->[2]}} ];
         }
         for my $i (0..$#edges) {
-            for my $j ($i+1..$#edges) {
+            EDGE: for my $j ($i+1..$#edges) {
                 my $path1 = $attributes->{$edges[$i]->[0]}{$edges[$i]->[1]}{$edges[$i]->[2]};
                 my $path2 = $attributes->{$edges[$j]->[0]}{$edges[$j]->[1]}{$edges[$j]->[2]};
                 # If paths have more vertices in common than $vertex, they have to be eliminated.
                 # $vertex will only participate in one of the paths if it is already visited and removed.
                 # This cannot already be done at the time of considering $vertex.
                 if( $path1 && $path2 ) {
-                    my $found;
                     for (keys %$path2) {
-                        next unless exists $path1->{$_};
-                        $found = 1;
-                        last;
+                        next EDGE if exists $path1->{$_};
                     }
-                    next if $found;
                 }
                 my @new_edge = sort grep { $_ ne $vertex }
                                 map { $_->[0], $_->[1] }
